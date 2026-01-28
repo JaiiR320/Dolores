8c0d032d06207f86e61605a06d8a682ca4deb41d
Jair Meza <jair3202@gmail.com>
2026-01-28 01:10:05 -0500

    Allow local dev origins

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  allowedDevOrigins: [
+    "http://192.168.1.181:3000",
+    "http://localhost:3000",
+  ],
 };
 
 export default nextConfig;
